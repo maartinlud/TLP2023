a404c14cd5f356172fef9d2fa26dc59c3e80656e
maartinlud <126704523+maartinlud@users.noreply.github.com>
2023-04-25 14:17:58 -0300

    Delete Ej31.py

--- Ej31.py
+++
@@ -1,7 +0,0 @@
-suma=0
-for x in range (11):
-   num= int (input ("Ingrese numero: "))
-   suma=suma+num
-promedio= suma/10
-print("la suma de los numeros es: ",suma)
-print("el promedio de los numeros es: ",promedio)
